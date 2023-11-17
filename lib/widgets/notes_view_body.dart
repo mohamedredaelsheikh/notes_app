d3b746773f8073819edd171fdeb3452919357c90
Mohammed_Elsheikh <moh.reda198@gmail.com>
2023-11-17 09:46:24 +0200

    complete custom note item

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/widgets/custom_appbar.dart';
-import 'package:note_app/widgets/note_item.dart';
+import 'package:note_app/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
